c16481f847857d4406e41a2023e761f9a22371d2
PiotrAntolik <antolcrew@gmail.com>
2020-06-06 18:54:32 +0200

    Updates

---
+++ precache-manifest.ca49659bd392b4378a9141d0574fef24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcc95bb40588da2f557dc0a8bdc67de",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "fc63900bdaf17fbffa82",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc63900bdaf17fbffa82",
+    "url": "/react-deploy/static/js/main.400a63c0.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
